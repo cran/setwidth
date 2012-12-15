b286034b2782a9d81d43e672729753b7f26f033b
Jakson Alves de Aquino <jalvesaq@gmail.com>
2012-12-15 00:00:00 +0000

    version 1.0-1

--- src/setwidth.c
+++ src/setwidth.c
@@ -1,14 +1,16 @@
 
 #include <R.h>  /* to include Rconfig.h */
 #include <Rinternals.h>
-#include <Rinterface.h>
+#include <R_ext/eventloop.h>
 
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <signal.h>
 
-int setwidth_initialized = 0;
-int setwidth_verbose = 0;
+static int setwidth_initialized = 0;
+static int setwidth_verbose = 0;
+static long oldcolwd = 0;
+static int ifd, ofd;
 
 
 void setwidth_Set()
@@ -33,20 +35,23 @@ void setwidth_Set()
 # endif
 
     if(columns > 0){
+        if(columns != oldcolwd){
+            oldcolwd = columns;
 
-        /* From R-exts: Evaluating R expressions from C */
-        SEXP s, t;
-        PROTECT(t = s = allocList(2));
-        SET_TYPEOF(s, LANGSXP);
-        SETCAR(t, install("options"));
-        t = CDR(t);
-        SETCAR(t, ScalarInteger((int)columns));
-        SET_TAG(t, install("width"));
-        eval(s, R_GlobalEnv);
-        UNPROTECT(1);
+            /* From R-exts: Evaluating R expressions from C */
+            SEXP s, t;
+            PROTECT(t = s = allocList(2));
+            SET_TYPEOF(s, LANGSXP);
+            SETCAR(t, install("options"));
+            t = CDR(t);
+            SETCAR(t, ScalarInteger((int)columns));
+            SET_TAG(t, install("width"));
+            eval(s, R_GlobalEnv);
+            UNPROTECT(1);
 
-        if(setwidth_verbose > 2)
-            Rprintf("setwidth: %d columns\n", columns);
+            if(setwidth_verbose > 2)
+                Rprintf("setwidth: %d columns\n", columns);
+        }
     } else {
         if(setwidth_verbose > 1)
             REprintf("Error on 'setwidth' package: could not detect the terminal width.\n");
@@ -55,18 +60,42 @@ void setwidth_Set()
 
 void handle_winch(int sig){
     signal(SIGWINCH, SIG_IGN);
-    setwidth_Set();
+    char buf[16];
+    *buf = 0;
+    if(write(ofd, buf, 16) <= 0)
+        REprintf("setwidth error: write <= 0\n");
     signal(SIGWINCH, handle_winch);
+}
+
+/* Code adapted from CarbonEL.
+ * Thanks to Simon Urbanek for the suggestion on r-devel mailing list. */
+static void uih(void *data) {
+  char buf[16];
+  if(read(ifd, buf, 16) == 0)
+      REprintf("setwidth error: read = 0\n");
+  R_ToplevelExec(setwidth_Set, NULL);
 }
 
 void setwidth_Start(int *verbose)
 {
     setwidth_verbose = *verbose;
-    setwidth_initialized = 1;
+
     if(setwidth_verbose)
-        REprintf("setwidth 1.0-0 loaded\nCopyright (C) 2011 Jakson A. Aquino\n"); 
+        REprintf("setwidth 1.0-1 loaded\nCopyright (C) 2012 Jakson A. Aquino\n"); 
+
     setwidth_Set();
     signal(SIGWINCH, handle_winch);
+
+    int fds[2];
+    if(pipe(fds) == 0){
+        ifd = fds[0];
+        ofd = fds[1];
+        addInputHandler(R_InputHandlers, ifd, &uih, 32);
+    } else {
+        REprintf("setwidth error: pipe != 0\n");
+    }
+
+    setwidth_initialized = 1;
 }
 
 void setwidth_Stop()
